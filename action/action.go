b60f1667221d48b0f44b690eb3b267b885a64c70
Xu Dongyan <dongyanxu@189.cn>
2023-03-17 13:37:42 +0800

    add refresh config by sending HUP signal

--- action/action.go
+++ action/action.go
@@ -6,27 +6,28 @@ import (
 	"os/exec"
 )
 
-func Action(servcieName string, configPath string, gitTag string) error {
+func Action(serviceName string, configPath string, gitTag string) error {
 	var err = os.Chdir(configPath)
 	if err != nil {
-		return fmt.Errorf("failed to change the dir of %s, error: %v", servcieName, err)
+		return fmt.Errorf("failed to change the dir of %s, error: %v", serviceName, err)
 	}
 
-	var gitCmd = fmt.Sprintf("git pull -f origin %s", gitTag)
-	if err := runCmd(gitCmd); err != nil {
+	if err := runCmd("git", "pull", "-f", "origin", gitTag); err != nil {
 		return err
 	}
 
-	var systemctlCmd = fmt.Sprintf("systemctl kill --signal=HUP %s", servcieName)
-	if err := runCmd(systemctlCmd); err != nil {
+	if err := runCmd("/usr/bin/systemctl", "kill", "--signal=HUP", serviceName); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func runCmd(commandline string) error {
-	var cmd = exec.Command(commandline)
+func runCmd(commandline string, args ...string) error {
+	var cmd = exec.Command(commandline, args...)
+	fmt.Println(cmd.String())
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
 	var err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("failed to run  '%s', error: %v", commandline, err)
--- config/config.go
+++ config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
 type Config struct {
 	Services []Service
 }
@@ -7,4 +14,29 @@ type Config struct {
 type Service struct {
 	Name       string `yaml:"name"`
 	ConfigPath string `yaml:"configPath"`
+}
+
+var GlobalConfig *Config
+
+func LoadConfig() error {
+	var configFile = flag.String("c", "", "the yaml config file")
+	flag.Parse()
+
+	var f, err = os.Open(*configFile)
+	if err != nil {
+		fmt.Printf("Failed to open config file, %s, error: %v", configFile, err)
+		return nil
+	}
+	defer f.Close()
+
+	var config Config
+	err = yaml.NewDecoder(f).Decode(&config)
+	if err != nil {
+		fmt.Printf("Failed to read yaml content, error: %v", err)
+		return nil
+	}
+
+	fmt.Printf("config file %s loaded", *configFile)
+	GlobalConfig = &config
+	return nil
 }
